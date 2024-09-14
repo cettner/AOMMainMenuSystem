60353591a53b647e4d1085526f1def7d6ffd5b20
cettner <chettner@gmail.com>
2024-09-14 12:06:03 -0500

    Adding Build and Module File

---
+++ MainMenuSystem/MainMenuSystem.Build.cs
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+
+public class MainMenuSystem : ModuleRules
+{
+	public MainMenuSystem(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "Engine", "CoreUObject" });
+
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
+	}
+}
